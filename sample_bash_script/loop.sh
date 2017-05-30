17876aed7f20add1ef2b8ca1dd9e8580e023880a
Aurora Cid <a.cid795@gmail.com>
2017-05-30 14:23:57 -0400

    Added my first bash loop

---
+++ sample_bash_script/loop.sh
@@ -0,0 +1,9 @@
+#comments are preceded by the hash symbol. You can use comments as documnetation, or just to temporarily remove lines from being executed.
+#This is a sample bash loop in bash.
+for i in {1..10};
+do echo $i;
+#do next iteration;
+for j in {a..g};
+do echo $j;
+done
+done
